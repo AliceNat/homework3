fe6a5f1834c494ad365af54423b8b6c6dbb33e1e
Alice Simonyan <alicesimonyan95@gmail.com>
2023-07-09 17:08:47 +0400

    homework3

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+     byte a = 127;
+     short b=32767;
+     int c= 2147483647;
+     long d=9223372036854775807L;
+
+     System.out.println("this is the largest value for the BYTE variable a= " +a );
+        System.out.println("this is the largest value for the SHORT variable b= "+b);
+            System.out.println("this is the largest value for the INT variable c= "+c);
+                System.out.println("this is the largest value for the LONG variable d= "+d);
+
+
+    }
+}
